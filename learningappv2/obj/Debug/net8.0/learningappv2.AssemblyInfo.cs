11cba72df3825d8b9ce4a493f0d0399f9155dbf1
Matthias Krenz <matthias.krenz@hotmail.com>
2024-10-13 20:36:21 +0200

    Modified Index.cshtml

--- learningappv2/obj/Debug/net8.0/learningappv2.AssemblyInfo.cs
+++ learningappv2/obj/Debug/net8.0/learningappv2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learningappv2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6636b069f63bfc42f895cb7be9022d27ff437e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("learningappv2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learningappv2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
